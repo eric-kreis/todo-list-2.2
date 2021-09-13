b6f6fcb77d6ece6d93e2cff5c6ef940edd671346
Eric Kreis <erickreis793@gmail.com>
2021-09-13 20:13:33 -0300

    [ Response ]

--- src/App.js
+++ src/App.js
@@ -15,8 +15,10 @@ export default function App() {
   const theme = useSelector(({ changeTheme }) => changeTheme.theme);
   const active = useSelector(({ sideBar }) => sideBar.active);
 
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
   window.addEventListener('resize', () => {
-    const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
   });
 
